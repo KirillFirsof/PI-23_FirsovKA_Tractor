3148aad12b24137d0b4cd89d373fe9d351eda403
KirillFirsof <117719052+KirillFirsof@users.noreply.github.com>
2023-09-17 20:37:37 +0400

    Изменение

--- Laba1RPP/Laba1RPP/FormTractor.cs
+++ Laba1RPP/Laba1RPP/FormTractor.cs
@@ -8,6 +8,16 @@ namespace Laba1RPP
 {
     public partial class FormTractor : Form
     {
+        private StatusStrip statusStrip1;
+        private ToolStripStatusLabel toolStripStatusLabelSpeed;
+        private ToolStripStatusLabel toolStripStatusLabelWeight;
+        private ToolStripStatusLabel toolStripStatusLabelColor;
+        private PictureBox pictureBoxTractor;
+        private Button buttonRight;
+        private Button buttonUp;
+        private Button buttonDown;
+        private Button buttonLeft;
+        private Button buttonCreate;
         private DrawningTractor _car;
 
         public FormTractor()
@@ -19,10 +29,10 @@ namespace Laba1RPP
         /// </summary>
         private void Draw()
         {
-            Bitmap bmp = new(pictureBoxCar.Width, pictureBoxCar.Height);
+            Bitmap bmp = new(pictureBoxTractor.Width, pictureBoxTractor.Height);
             Graphics gr = Graphics.FromImage(bmp);
             _car?.DrawTransport(gr);
-            pictureBoxCar.Image = bmp;
+            pictureBoxTractor.Image = bmp;
         }
         /// <summary>
         /// Обработка нажатия кнопки "Создать"
@@ -32,16 +42,14 @@ namespace Laba1RPP
         private void ButtonCreate_Click(object sender, EventArgs e)
         {
             Random rnd = new();
-            _car = new DrawningCar();
+            _car = new DrawningTractor();
             _car.Init(rnd.Next(100, 300), rnd.Next(1000, 2000),
            Color.FromArgb(rnd.Next(0, 256), rnd.Next(0, 256), rnd.Next(0, 256)));
             _car.SetPosition(rnd.Next(10, 100), rnd.Next(10, 100),
-           pictureBoxCar.Width, pictureBoxCar.Height);
-            toolStripStatusLabelSpeed.Text = $"Скорость: {_car.Car.Speed}";
-            toolStripStatusLabelWeight.Text = $"Вес: {_car.Car.Weight}";
-            toolStripStatusLabelBodyColor.Text = $"Цвет:
-       { _car.Car.BodyColor.Name}
-            ";
+           pictureBoxTractor.Width, pictureBoxTractor.Height);
+            toolStripStatusLabelSpeed.Text = $"Скорость: {_car.Tractor.Speed}";
+            toolStripStatusLabelWeight.Text = $"Вес: {_car.Tractor.Weight}";
+            toolStripStatusLabelColor.Text = $"Цвет: { _car.Tractor.BodyColor.Name}";
         Draw();
         }
         /// <summary>
@@ -77,8 +85,150 @@ namespace Laba1RPP
         /// <param name="e"></param>
         private void PictureBoxCar_Resize(object sender, EventArgs e)
         {
-            _car?.ChangeBorders(pictureBoxCar.Width, pictureBoxCar.Height);
+            _car?.ChangeBorders(pictureBoxTractor.Width, pictureBoxTractor.Height);
             Draw();
         }
+
+        private void InitializeComponent()
+        {
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabelSpeed = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabelWeight = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabelColor = new System.Windows.Forms.ToolStripStatusLabel();
+            this.pictureBoxTractor = new System.Windows.Forms.PictureBox();
+            this.buttonRight = new System.Windows.Forms.Button();
+            this.buttonUp = new System.Windows.Forms.Button();
+            this.buttonDown = new System.Windows.Forms.Button();
+            this.buttonLeft = new System.Windows.Forms.Button();
+            this.buttonCreate = new System.Windows.Forms.Button();
+            this.statusStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxTractor)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabelSpeed,
+            this.toolStripStatusLabelWeight,
+            this.toolStripStatusLabelColor});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 427);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(882, 26);
+            this.statusStrip1.TabIndex = 0;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabelSpeed
+            // 
+            this.toolStripStatusLabelSpeed.Name = "toolStripStatusLabelSpeed";
+            this.toolStripStatusLabelSpeed.Size = new System.Drawing.Size(73, 20);
+            this.toolStripStatusLabelSpeed.Text = "Скорость";
+            // 
+            // toolStripStatusLabelWeight
+            // 
+            this.toolStripStatusLabelWeight.Name = "toolStripStatusLabelWeight";
+            this.toolStripStatusLabelWeight.Size = new System.Drawing.Size(33, 20);
+            this.toolStripStatusLabelWeight.Text = "Вес";
+            // 
+            // toolStripStatusLabelColor
+            // 
+            this.toolStripStatusLabelColor.Name = "toolStripStatusLabelColor";
+            this.toolStripStatusLabelColor.Size = new System.Drawing.Size(42, 20);
+            this.toolStripStatusLabelColor.Text = "Цвет";
+            // 
+            // pictureBoxTractor
+            // 
+            this.pictureBoxTractor.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBoxTractor.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBoxTractor.Location = new System.Drawing.Point(0, 0);
+            this.pictureBoxTractor.Name = "pictureBoxTractor";
+            this.pictureBoxTractor.Size = new System.Drawing.Size(882, 427);
+            this.pictureBoxTractor.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBoxTractor.TabIndex = 1;
+            this.pictureBoxTractor.TabStop = false;
+            this.pictureBoxTractor.Click += new System.EventHandler(this.pictureBoxTractor_Click);
+            // 
+            // buttonRight
+            // 
+            this.buttonRight.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonRight.BackgroundImage = global::Laba1RPP.Properties.Resources.arrowRight;
+            this.buttonRight.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.buttonRight.Location = new System.Drawing.Point(825, 387);
+            this.buttonRight.Name = "buttonRight";
+            this.buttonRight.Size = new System.Drawing.Size(30, 30);
+            this.buttonRight.TabIndex = 2;
+            this.buttonRight.UseVisualStyleBackColor = true;
+            this.buttonRight.Click += new System.EventHandler(this.ButtonMove_Click);
+            // 
+            // buttonUp
+            // 
+            this.buttonUp.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonUp.BackgroundImage = global::Laba1RPP.Properties.Resources.arrowUp;
+            this.buttonUp.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.buttonUp.Location = new System.Drawing.Point(789, 351);
+            this.buttonUp.Name = "buttonUp";
+            this.buttonUp.Size = new System.Drawing.Size(30, 30);
+            this.buttonUp.TabIndex = 3;
+            this.buttonUp.UseVisualStyleBackColor = true;
+            this.buttonUp.Click += new System.EventHandler(this.ButtonMove_Click);
+            // 
+            // buttonDown
+            // 
+            this.buttonDown.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonDown.BackgroundImage = global::Laba1RPP.Properties.Resources.arrowDown;
+            this.buttonDown.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.buttonDown.Location = new System.Drawing.Point(789, 387);
+            this.buttonDown.Name = "buttonDown";
+            this.buttonDown.Size = new System.Drawing.Size(30, 30);
+            this.buttonDown.TabIndex = 4;
+            this.buttonDown.UseVisualStyleBackColor = true;
+            this.buttonDown.Click += new System.EventHandler(this.ButtonMove_Click);
+            // 
+            // buttonLeft
+            // 
+            this.buttonLeft.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonLeft.BackgroundImage = global::Laba1RPP.Properties.Resources.arrowLeft;
+            this.buttonLeft.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.buttonLeft.Location = new System.Drawing.Point(753, 387);
+            this.buttonLeft.Name = "buttonLeft";
+            this.buttonLeft.Size = new System.Drawing.Size(30, 30);
+            this.buttonLeft.TabIndex = 5;
+            this.buttonLeft.UseVisualStyleBackColor = true;
+            this.buttonLeft.Click += new System.EventHandler(this.ButtonMove_Click);
+            // 
+            // buttonCreate
+            // 
+            this.buttonCreate.Location = new System.Drawing.Point(8, 386);
+            this.buttonCreate.Name = "buttonCreate";
+            this.buttonCreate.Size = new System.Drawing.Size(94, 29);
+            this.buttonCreate.TabIndex = 6;
+            this.buttonCreate.Text = "Создание";
+            this.buttonCreate.UseVisualStyleBackColor = true;
+            this.buttonCreate.Click += new System.EventHandler(this.ButtonCreate_Click);
+            // 
+            // FormTractor
+            // 
+            this.ClientSize = new System.Drawing.Size(882, 453);
+            this.Controls.Add(this.buttonCreate);
+            this.Controls.Add(this.buttonLeft);
+            this.Controls.Add(this.buttonDown);
+            this.Controls.Add(this.buttonUp);
+            this.Controls.Add(this.buttonRight);
+            this.Controls.Add(this.pictureBoxTractor);
+            this.Controls.Add(this.statusStrip1);
+            this.Name = "FormTractor";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxTractor)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+        private void pictureBoxTractor_Click(object sender, EventArgs e)
+        {
+
+        }
+
     }
 }
